6f060ce303b9143f20a3f562d506a7b086e9a672
Jessica Machado <machadojessica1997@gmail.com>
2026-01-21 14:59:04 -0300

    "feat: criando autenticação"

--- src/main/java/com/api/financial/management/domain/entity/Usuario.java
+++ src/main/java/com/api/financial/management/domain/entity/Usuario.java
@@ -1,33 +1,45 @@
 package com.api.financial.management.domain.entity;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Usuario {
 
-    private final UUID id;
-    private final String nome;
-    private final String email;
-    private final String login;
-    private final String senha;
+    private final  UUID id;
+    private String nome;
+    private String email;
+    private DadosAutenticacao autenticacao;
 
-    public Usuario(UUID id, String nome, String email, String login, String senha) {
+    public Usuario(UUID id, String name, String email, DadosAutenticacao autenticacao) {
+        this.id = id;
+        this.nome = name;
+        this.email = email;
+        this.autenticacao = autenticacao;
+        this.validar();
+    }
 
+    private void validar() {
         if (nome == null || nome.isBlank()) {
             throw new IllegalArgumentException("Nome não pode ser vazio");
         }
-
-        if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException("Email não pode ser vazio");
-        }
-
-        if (login == null || login.isBlank()) {
-            throw new IllegalArgumentException("Login não pode ser vazio");
-        }
-
-        this.id = UUID.randomUUID();
-        this.nome = nome;
-        this.email = email;
-        this.login = login;
-        this.senha = senha;
     }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Usuario user = (Usuario) obj;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getLogin() {
+        return autenticacao.login();
+    }
+
 }
