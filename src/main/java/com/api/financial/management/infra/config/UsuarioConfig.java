1ec2b647d1712cbcb294b363c4388fc51a7c83aa
Jessica Machado <machadojessica1997@gmail.com>
2026-01-22 11:47:49 -0300

    "feat: implementação listagem + configurações de teste"

--- src/main/java/com/api/financial/management/infra/config/UsuarioConfig.java
+++ src/main/java/com/api/financial/management/infra/config/UsuarioConfig.java
@@ -1,6 +1,7 @@
 package com.api.financial.management.infra.config;
 
 import com.api.financial.management.application.usecase.CriarUsuario;
+import com.api.financial.management.application.usecase.ListarUsuario;
 import com.api.financial.management.domain.repository.UsuarioRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,4 +13,10 @@ public class UsuarioConfig {
     public CriarUsuario criarUsuario(UsuarioRepository usuarioRepository){
         return new CriarUsuario(usuarioRepository);
     }
+
+    @Bean
+    public ListarUsuario listarUsuario(UsuarioRepository usuarioRepository) {
+        return new ListarUsuario(usuarioRepository);
+    }
+
 }
