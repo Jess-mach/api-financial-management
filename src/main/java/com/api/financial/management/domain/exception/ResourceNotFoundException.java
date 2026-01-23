c447912ad875ea2652cf4a3260f3d1a862a9eac0
Jessica Machado <machadojessica1997@gmail.com>
2026-01-23 16:04:40 -0300

    "feat: Correção de autenticação.

--- src/main/java/com/api/financial/management/domain/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.api.financial.management.domain.exception;
-
-class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
