00a3c394fb079ec2e233c1b88adc472a98f0578d
Jessica Machado <machadojessica1997@gmail.com>
2026-01-28 04:35:35 -0300

    feat: Autenticação

---
+++ transacao-producer-api/src/main/java/br/com/ntt/transacao/producer/config/SecurityFilter.java
@@ -0,0 +1,63 @@
+package br.com.ntt.transacao.producer.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        var tokenJWT = recuperarToken(request);
+
+        if (tokenJWT != null) {
+            try {
+                var subject = getSubject(tokenJWT);
+                var authentication = new UsernamePasswordAuthenticationToken(subject, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getSubject(String tokenJWT) {
+        var algorithm = Algorithm.HMAC256(secret);
+        return JWT.require(algorithm)
+                .withIssuer("API Financial.management")
+                .build()
+                .verify(tokenJWT)
+                .getSubject();
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
