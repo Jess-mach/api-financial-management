1cece80680d429654aedda43f82b3e6626a7ad50
Jessica Machado <machadojessica1997@gmail.com>
2026-01-30 08:00:32 -0300

    fix: correção da SQL

--- common-transacao-lib/src/main/java/br/com/ntt/common/transacao/infra/persistence/TransacaoRepository.java
+++ common-transacao-lib/src/main/java/br/com/ntt/common/transacao/infra/persistence/TransacaoRepository.java
@@ -2,6 +2,7 @@ package br.com.ntt.common.transacao.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,18 +10,17 @@ import java.util.UUID;
 public interface TransacaoRepository extends JpaRepository<TransacaoEntity, UUID> {
 
     @Query(value = """
-                 select 
-                         DATE(t.data_hora_solicitacao) as dataHoraSolicitacao,
-                         t.tipo as tipo ,
-                         sum (t.valor) as valor,
-                         count(*) as quantidade
-                 from transacoes t
-                 where t.usuario_id = ?1
-                 and t.status = 1
-                 group by t.tipo , DATE(data_hora_solicitacao)
-        
-            """, nativeQuery = true)
-    List<AnaliseDeDespesaCampos> visualisarGastosDia(UUID usuarioId);
+             select 
+                     CAST(DATE(t.dataHoraSolicitacao) AS text) as dataHoraSolicitacao,
+                     CAST(t.tipo AS int) as tipo,
+                     CAST(sum(t.valor) as double) as valor,
+                     CAST(count(t) as int) as quantidade
+             from TransacaoEntity t
+             where t.usuarioId = :usuarioId
+             and t.status = br.com.ntt.common.transacao.domain.model.StatusTransacao.AUTORIZADO
+             group by t.tipo,  DATE(dataHoraSolicitacao)
+        """)
+    List<AnaliseDeDespesaCampos> visualisarGastosDia(@Param("usuarioId") UUID usuarioId);
 
     @Query(value = """
                  select
